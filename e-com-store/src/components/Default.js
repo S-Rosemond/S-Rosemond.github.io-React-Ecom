a938d85a0035f1a36351e8455a27bac9cb83469b
Samuel <js.rosemond@gmail.com>
2019-04-24 21:50:19 -0400

    paypal, .env.variables, course complete

--- e-com-store/src/components/Default.js
+++ e-com-store/src/components/Default.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 export default class Default extends Component {
 	render() {
 		return (
-			<div>
-				<h3>Page not Found</h3>
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+						<h1 className="display-3">404</h1>
+						<h2>error: page Not found</h2>
+						<h4>
+							the requested url:
+							<span className="text-primary">{this.props.location.pathname}</span>was not found
+						</h4>
+					</div>
+				</div>
 			</div>
 		);
 	}
